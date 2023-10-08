ef2241deaf12a2f39ba09efa46d333c30e7ce891
Vladislav <kovleshenko99@mail.ru>
2023-10-08 20:51:32 +0400

    added password confirm and validation

--- app/(auth)/sign-up/components/sign-up.tsx
+++ app/(auth)/sign-up/components/sign-up.tsx
@@ -45,6 +45,10 @@ function SignUp() {
             {
               required: true,
               message: 'Please, input your email!'
+            },
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!'
             }
           ]}
         >
@@ -60,6 +64,32 @@ function SignUp() {
               required: true,
               message: 'Please, input your password!'
             }
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          className="w-full"
+          label="Confirm password"
+          name="confirm-password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please, confirm your password!'
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve()
+                }
+                return Promise.reject(
+                  new Error('The new password that you entered do not match!')
+                )
+              }
+            })
           ]}
         >
           <Input.Password />
