0d6d147d53da927fdbdbdb1d905e2339ee3cd44b
Vladislav <kovleshenko99@mail.ru>
2023-11-22 21:13:11 +0400

    fix prepender cookies part 2

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,12 +4,20 @@ export async function GET(req: NextRequest) {
   try {
     //TODO: Refactor token hack
     const token = req.cookies.get('__token')?.value
+
+    if (!token) {
+      return NextResponse.json({status: 400})
+    }
     const config = token ? {
       headers: {Authorization: `Bearer ${token}`}
     } : {}
-    const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_BACKEND_API_URL}/users/me`, config)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_BACKEND_API_URL}/users/me`, config)
     
-    return NextResponse.json(await data.json());
+    if (res.ok) {
+      return NextResponse.json(await res.json());
+    } else {
+      throw new NextResponse('Internal error', { status: 400 })
+    }
   } catch (e) {
     console.log(e)
     throw new NextResponse('Internal error', { status: 400 })
